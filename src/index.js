1131c996b7917ae85a5eb0fb1b77189f4d4aaf21
Anderson Aquino <andersonaquino93@gmail.com>
2021-07-22 19:50:40 -0300

    add customer exists validation

--- src/index.js
+++ src/index.js
@@ -33,6 +33,10 @@ app.get("/statement/:cpf", (request, response) => {
 
   const customer = customers.find((customer) => customer.cpf === cpf)
 
+  if (!customer) {
+    return response.status(404).json({ error: "Customer not found!" })
+  }
+
   return response.json(customer.statement)
 })
 
