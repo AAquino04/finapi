e3e59d80b5036f17e5012dbaa1b901d1a9447404
Anderson Aquino <andersonaquino93@gmail.com>
2021-07-26 14:18:38 -0300

    add deposit feature

--- src/index.js
+++ src/index.js
@@ -41,6 +41,23 @@ app.post("/account", (request, response) => {
   return response.status(201).send()
 })
 
+app.post("/deposit", verifyIfAccountExistsByCPF, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
 app.get("/statement", verifyIfAccountExistsByCPF, (request, response) => {
   const { customer } = request
 
