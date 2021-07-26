f1e0929b685f369106e44f481d474e4a677229a9
Anderson Aquino <andersonaquino93@gmail.com>
2021-07-26 17:29:38 -0300

    add delete account feature

--- src/index.js
+++ src/index.js
@@ -129,4 +129,12 @@ app.put("/account", verifyIfAccountExistsByCPF, (request, response) => {
   return response.status(200).send()
 })
 
+app.delete("/account", verifyIfAccountExistsByCPF, (request, response) => {
+  const { customer } = request
+
+  customers.splice(customer, 1)
+
+  return response.status(200).json(customers)
+})
+
 app.listen(3333)
